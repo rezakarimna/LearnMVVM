2773d5789f6df83c225e22d127ce0abe32b1133b
rezakarimna <karimnia.reza70@yahoo.com>
2021-03-30 22:02:07 +0430

    MVVM part3

---
+++ app/src/main/java/com/learnmvvm/SimpleDataBindingActivity.java
@@ -0,0 +1,27 @@
+package com.learnmvvm;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.learnmvvm.databinding.ActivityBindingDataSimpleBinding;
+import com.learnmvvm.model.Users;
+
+public class SimpleDataBindingActivity extends AppCompatActivity {
+    ActivityBindingDataSimpleBinding binding;
+    Users users = new Users();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_binding_data_simple);
+        binding= DataBindingUtil.setContentView(this , R.layout.activity_binding_data_simple);
+        users.setName("REZA KARIMNIA");
+        binding.setUsers(users);
+    }
+
+    private void  b(){
+        Toast.makeText(this, "github", Toast.LENGTH_SHORT).show();
+    }
+}
---
+++ app/src/main/java/com/learnmvvm/model/Users.java
@@ -0,0 +1,14 @@
+package com.learnmvvm.model;
+
+public class Users {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
