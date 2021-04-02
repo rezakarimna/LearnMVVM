eebbd6ba23c85248789b89ebfcf087671b17927a
rezakarimna <karimnia.reza70@yahoo.com>
2021-04-02 18:53:58 +0430

    Mvvm part05

--- app/src/main/java/com/learnmvvm/adapter/AdapterRecyclerView.java
+++ app/src/main/java/com/learnmvvm/adapter/AdapterRecyclerView.java
@@ -1,28 +1,44 @@
 package com.learnmvvm.adapter;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.learnmvvm.R;
 import com.learnmvvm.databinding.ItemRecyclerviewBinding;
+import com.learnmvvm.model.Users;
+
+import java.util.List;
 
 public class AdapterRecyclerView extends RecyclerView.Adapter<AdapterRecyclerView.MyViewHolder> {
+    Context context;
+    List<Users> data ;
+
+    public AdapterRecyclerView(Context context, List<Users> data) {
+        this.context = context;
+        this.data = data;
+    }
+
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        ItemRecyclerviewBinding itemRecyclerviewBinding = DataBindingUtil.inflate(LayoutInflater.from(context) , R.layout.item_recyclerview , parent, false);
+        return new MyViewHolder(itemRecyclerviewBinding);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-
+        holder.itemRecyclerviewBinding.setUsers(data.get(position));
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return data.size();
     }
 
     public static class MyViewHolder extends RecyclerView.ViewHolder {
--- app/src/main/java/com/learnmvvm/model/Users.java
+++ app/src/main/java/com/learnmvvm/model/Users.java
@@ -1,8 +1,20 @@
 package com.learnmvvm.model;
 
+import android.widget.ImageView;
+
+import androidx.databinding.BindingAdapter;
+import androidx.databinding.BindingMethod;
+
+import com.squareup.picasso.Picasso;
+
 public class Users {
 
-    private String name, email;
+    private String name, email, linkImg;
+
+    @BindingAdapter({"android:imageUser"})
+    public static void loadImageUser(ImageView imgUser, String linkImg) {
+        Picasso.get().load(linkImg).into(imgUser);
+    }
 
     public String getName() {
         return name;
@@ -18,5 +30,13 @@ public class Users {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getLinkImg() {
+        return linkImg;
+    }
+
+    public void setLinkImg(String linkImg) {
+        this.linkImg = linkImg;
     }
 }
